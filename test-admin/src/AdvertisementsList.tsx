72f7a2bbd627ac003b192f6ab6b209ba38ccbeab
23jz0147 <23jz0147@jec.ac.jp>
2025-10-17 12:17:04 +0900

    deleteMany実装

--- test-admin/src/AdvertisementsList.tsx
+++ test-admin/src/AdvertisementsList.tsx
@@ -1,18 +1,24 @@
-import { Datagrid, List, TextField, DateField, Filter, TextInput } from "react-admin";
+import { Datagrid, TextField, DateField, List, SimpleList } from "react-admin";
+import { useMediaQuery } from "@mui/material";
 
-const AdvertisementFilter = (props) => (
-  <Filter {...props}>
-    <TextInput label="年号" source="year" alwaysOn />
-  </Filter>
-);
+export const AdvertisementsList = () => {
 
-export const AdvertisementsList = (props) => (
-  <List {...props} filters={<AdvertisementFilter />}>
-    <Datagrid rowClick="edit">
-      <TextField source="id" label="ID" />
-      <TextField source="company_name" label="会社名" />
-      <TextField source="company_id" label="会社ID" />
-      <DateField source="updated_at" label="更新日" />
-    </Datagrid>
-  </List>
+const isSmall = useMediaQuery((theme) => theme.breakpoints.down("sm")); // 画面幅が "sm" 以下の場合に true
+
+return (
+    <List>
+        {isSmall ? (
+            <SimpleList
+                primaryText={(record) => record.account_name}
+                secondaryText={(record) => record.company_name}/>
+        ) : (
+            <Datagrid rowClick="edit">
+                <TextField source="id" label="ID" />
+                <TextField source="company_name" label="会社名" />
+                <TextField source="company_id" />
+                <DateField source="updated_at" label="更新日" />
+            </Datagrid>
+        )}
+    </List>
 );
+};
